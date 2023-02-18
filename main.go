1489893df99320ee0997269960f4fac2508112d3
emeka-chukwu <emekapascal11@gmail.com>
2023-02-18 14:15:21 +0100

    viper-reader

--- main.go
+++ main.go
@@ -5,25 +5,30 @@ import (
 	"log"
 	"simplebank/api"
 	db "simplebank/db/sqlc"
+	"simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
