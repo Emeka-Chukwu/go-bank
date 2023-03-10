9fb30cf070ae47baa686368247376a8e6cb8efd3
emeka-chukwu <emekapascal11@gmail.com>
2023-03-10 06:01:48 +0100

    hashed password

--- api/server.go
+++ api/server.go
@@ -24,6 +24,7 @@ func NewServer(store db.Store) *Server {
 	}
 
 	router.POST("/accounts", server.createAccount)
+	router.POST("/users", server.createUser)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.POST("/transfers", server.createTransfer)
---
+++ api/user.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"net/http"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=5"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	CreatedAt         time.Time `json:"created_at"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorrResponse(err))
+		return
+	}
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorrResponse(err))
+		return
+	}
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorrResponse((err)))
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorrResponse((err)))
+		return
+	}
+
+	rsp := createUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, rsp)
+
+}
