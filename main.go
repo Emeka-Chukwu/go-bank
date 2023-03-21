82e3d09563e1e775ed911ad94021968085fa9e51
emeka-chukwu <emekapascal11@gmail.com>
2023-03-21 18:06:33 +0100

    migration in code

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ import (
 	"simplebank/pb"
 	"simplebank/util"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -37,11 +40,26 @@ func main() {
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
+
+	/// run db migration
+	runDBMigration(config.MigrationURL, config.DBSource)
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 
 }
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance:", err)
+	}
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migration up:", err)
+	}
+	log.Println("db migrated successfully")
+}
+
 func runGrpcServer(config util.Config, store db.Store) {
 	server, err := gapi.NewServer(config, store)
 	if err != nil {
