c3a851be5e194ca23b7af70cb7c238b655e66c2c
emeka-chukwu <emekapascal11@gmail.com>
2023-06-19 13:40:13 +0100

    updates

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,7 +1,10 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	db "simplebank/db/sqlc"
+	"simplebank/token"
 	"simplebank/util"
 	"simplebank/worker"
 	"testing"
@@ -9,6 +12,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -19,4 +23,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	server, err := NewServer(config, store, taskDistributor)
 	require.NoError(t, err)
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
